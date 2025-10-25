4b180fe5725049dcc64b9da75066ccf27ccc5cab
Hari Maurya <4748625+harimaurya@users.noreply.github.com>
2025-10-25 17:47:19 +1300

    Removed redundant mock function

--- app/(auth)/register/page.test.tsx
+++ app/(auth)/register/page.test.tsx
@@ -1,12 +1,6 @@
-import { describe, expect, test, vi } from "vitest";
+import { describe, expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
 import RegisterPage from "./page";
-
-// Mock next/navigation -> useRouter
-const mockRouter = { replace: vi.fn() };
-vi.mock("next/navigation", () => ({
-  useRouter: () => mockRouter,
-}));
 
 describe("Register Page", () => {
   test("should renders the logo image with expected attributes", () => {
